14b4b8b910846cf2d4fb0746feb8d7919b8dddef
Santiago Zapata <santi@Santiagos-MacBook-Pro.local>
2023-07-11 15:53:30 -0300

    terminamos primer proyecto web :)

--- unidad 12/unidad7.js
+++ unidad 12/unidad7.js
@@ -1,13 +1,13 @@
 //formulario de datos personales
 
-/*
+
     var nombreCompleto = prompt("Ingrese su nombre completo:");
     var edad = prompt("ingrese su edad:");
     var localidad = prompt("ingrese su localidad:");
     var mail = prompt("ingrese su mail:");
     var intereses = prompt("¿Que es lo que lo/la trae a este sitio?", "ej: entrenamiento");
  
-*/
+
 
 
 
